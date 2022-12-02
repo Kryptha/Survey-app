1ff437c35bc403f586344482ca5b814ab67f2f6f
Kryptha <gema.espinoza@usach.cl>
2022-12-02 20:31:24 -0300

    Preparing an initial landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get 'pages/landing_page'
+  root 'pages#landing_page'
+  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

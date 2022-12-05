be7aef50d1b675ccdb7014fb75dec559eaab713e
Kryptha <gema.espinoza@usach.cl>
2022-12-05 17:44:10 -0300

    Add authentication forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#landing_page'
-  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  authenticated(:user) do
+    root 'pages#index', as: :authenticated_root
+  end
+
+  unauthenticated(:user) do
+    root 'pages#landing_page'
+  end
 end
